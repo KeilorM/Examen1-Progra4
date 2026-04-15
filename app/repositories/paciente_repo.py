8495ce43a9edf1a3cbc04f0f7b0c5ba73c2ae72d
Keilor Muñoz <munozkeylor4@gmail.com>
2026-04-14 22:38:28 -0600

    add paginacion y filtros

--- app/repositories/paciente_repo.py
+++ app/repositories/paciente_repo.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import Session
 from app.models import Paciente
 
-def get_all(db: Session):
-    return db.query(Paciente).all()
+def get_all(db: Session, skip: int = 0, limit: int = 10, nombre: str = None):
+    query = db.query(Paciente)
+    if nombre:
+        query = query.filter(Paciente.nombre_completo.ilike(f"%{nombre}%"))
+    return query.offset(skip).limit(limit).all()
 
 def get_by_id(db: Session, paciente_id: int):
     return db.query(Paciente).filter(Paciente.id == paciente_id).first()
